39d61ba9684513300696193886532c5a98b7ce2b
Nicole Wong <nrewong@gmail.com>
2020-06-10 01:10:54 +0900

    complete snapcarte info page

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,1 +1,11 @@
 //= require_tree .
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener("scroll", function() {
+    if (window.scrollY > 180) {
+        navbar.style.display = "flex";
+    }
+    else {
+        navbar.style.display = "none";
+    }
+},false);
